b05222375ed7712ad070cba988736d613ec6a20c
tcarman <tcarman@ucsd.edu>
2022-05-05 11:44:36 -0500

    Attemped to fix issues

--- grade.sh
+++ grade.sh
@@ -1,6 +1,6 @@
 
 git clone $1
-cp GradingTests.java markdown-parse/
-cd markdown-parse
+cp GradingTests.java markdown-parser/
+cd markdown-parser
 javac -cp lib/*:. GradingTests.java MarkdownParse.java
 java -cp lib/*:. org.junit.runner.JUnitCore GradingTests
